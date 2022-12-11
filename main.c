ce817e06af6ee3de676ea6f882c7e9db85e41163
Tereza Sukupová <tereza.sukupova@anafra.cz>
2022-12-11 15:17:48 +0100

    2-2

--- main.c
+++ main.c
@@ -8,28 +8,33 @@ int main() {
     lose = 0;
     draw = 3;
     int ascii[128] = {0};
-    ascii['X'] = 1;
-    ascii['Y'] = 2;
-    ascii['Z'] = 3;
+    ascii['X'] = 0;
+    ascii['Y'] = 3;
+    ascii['Z'] = 6;
+    ascii['A'] = 1;
+    ascii['B'] = 2;
+    ascii['C'] = 3;
+    ascii['D'] = 1;
+    ascii[64] = 3;
 
 
     FILE *f = fopen("/home/tereza/CLionProjects/aoc2022/input.txt", "r");
     char buff[256];
     while (fgets(buff, sizeof(buff), f)) {
         char enemy = buff[0];
+        int move;
         char me = buff[2];
         int diff = me-enemy-23;
-        if (diff == 0) {
-            printf("draw");
-            score += draw;
+        if (me == 'X') {
+           move =  enemy-1;
         }
-        else if (diff == 1 || diff == -2) {
-            score += win;
+        else if (me == 'Y') {
+            move = enemy;
         }
         else {
-            score += lose;
+            move = enemy +1;
         }
-        printf("%d %d %c %c\n",diff,ascii[me],enemy,me);
+        score += ascii[move];
         score += ascii[me];
     }
     printf("%d", score);
