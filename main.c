fdf009abdf24926f4f92d888ccc41cf288ccf74d
Tereza Sukupová <tereza.sukupova@anafra.cz>
2022-12-24 20:05:20 +0100

    15-1

--- main.c
+++ main.c
@@ -16,94 +16,52 @@ int main() {
     size_t lineSize;
     int count = 0;
     int yMax = 0;
+    int xB, yB, xS, yS;
+    char * equalsPtr = NULL;
+    int *  remove = NULL;
+    int removeC = 0;
+    int line = 2000000;
     GHashTable *ht = g_hash_table_new(g_str_hash, g_str_equal);
     while ((lineSize = getline(&linePtr, &n, f)) != -1) {
-        char *nCoord = NULL;
-        char *cSep = NULL;
-        char *a = strtok_r(linePtr, ",", &cSep);
-        char *b = strtok_r(NULL, ">", &cSep);
-        int column = (int) strtol(a, NULL, 10);
-        int row = (int) strtol(b, NULL, 10);
-        if (row > yMax) {
-            yMax = row;
+        strtok(linePtr, "=");
+        xS = (int) strtol(strtok(NULL, "="), NULL, 10);
+        yS = (int) strtol(strtok(NULL, "="), NULL, 10);
+        xB = (int) strtol(strtok(NULL, "="), NULL, 10);
+        yB = (int) strtol(strtok(NULL, "="), NULL, 10);
+        int d = abs(xS - xB) + abs(yS - yB);
+        int yD = d - abs(yS - line);
+//        if (yS == line) {
+//            removeC++;
+//            remove = realloc(remove, removeC * sizeof(int));
+//            remove[removeC - 1] =  xS;
+//        }
+        if (yB == line) {
+            removeC++;
+            remove = realloc(remove, removeC * sizeof(int));
+            remove[removeC - 1] =  xB;
         }
-        while (*cSep != '\0') {
-            a = strtok_r(NULL, ",", &cSep);
-            b = strtok_r(NULL, ">", &cSep);
-
-            int tCol = (int) strtol(a, NULL, 10);
-            int tRow = (int) strtol(b, NULL, 10);
-
-            if (tRow > yMax) {
-                yMax = tRow;
-            }
-            int ca = column;
-            int cb = tCol;
-            int ra = row;
-            int rb = tRow;
-
-            if (tCol > column) {
-                ca = tCol;
-                cb = column;
-            }
-            if (tRow > row) {
-                ra = tRow;
-                rb = row;
-            }
-            if (ca != cb) {
-                for (int i = cb; i <= ca; ++i) {
-                    char *s = calloc(8, sizeof(char));
-                    sprintf(s, "%d,%d", row, i);
-                    g_hash_table_add(ht, s);
-                }
-            }
-            if (ra != rb) {
-                for (int i = rb; i <= ra; ++i) {
-                    char *s = calloc(8, sizeof(char));
-                    sprintf(s, "%d,%d", i, column);
-                    g_hash_table_add(ht, s);
-                }
-            }
-
-            column = tCol;
-            row = tRow;
-        }
-    }
-    bool run = true;
-    while (run) {
-        int row = 0;
-        int col = 500;
-        int run2 = true;
-        while (run2) {
-            char *s = calloc(8, sizeof(char));
-            if (row > yMax) {
-                run = false;
-                run2 = false;
-            }
-            sprintf(s, "%d,%d", row+1, col);
-            if (g_hash_table_contains(ht, s)) {
-                sprintf(s, "%d,%d", row+1, col-1);
-                if (g_hash_table_contains(ht, s)) {
-                    sprintf(s, "%d,%d", row+1, col+1);
-                    if (g_hash_table_contains(ht, s)) {
-                        count++;
-                        sprintf(s, "%d,%d", row, col);
-                        g_hash_table_add(ht, s);
-                        run2 = false;
-                    }else {
-                        row++;
-                        col++;
-                    }
-                }else {
-                    row++;
-                    col--;
-                }
-            }else {
-                row++;
+        if (yD >= 0) {
+            for (int i = xS -  yD; i <= xS + yD; ++i) {
+                char *s = malloc(16);
+                sprintf(s, "%d", i);
+                g_hash_table_add(ht, s);
             }
         }
     }
-    printf("%d", count);
+    for (int i = 0; i < removeC; ++i) {
+        char *s = malloc(16);
+        sprintf(s, "%d", remove[i]);
+        g_hash_table_remove(ht, s);
+
+    }
+    for (int i = 0; i < 40; ++i) {
+        char * s = calloc(16, sizeof(char));
+        sprintf(s,"%d", i);
+        printf("%c", g_hash_table_contains(ht, s) ? '#' : '.');
+    }
+    printf("%d", g_hash_table_size(ht));
+    g_hash_table_destroy(ht);
+    free(linePtr);
 
     return 0;
 }
