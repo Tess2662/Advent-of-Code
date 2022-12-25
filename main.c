74e9ec0698bdab0a69abcfa131a11f29cfc85737
Tereza Sukupová <tereza.sukupova@anafra.cz>
2022-12-26 00:18:05 +0100

    19-1 failed better

--- main.c
+++ main.c
@@ -17,11 +17,19 @@ typedef struct {
     int bots[4];
 } Blueprint;
 
+typedef struct {
+    int mRounds;
+    int mResource;
+    int mParent;
+} Bottleneck;
+
 char *linePtr = NULL;
 size_t n = 0;
 size_t lineSize;
 int count = 0;
-//Blueprint bp[30];
+Blueprint bp;
+int btc = 0;
+Bottleneck bottlenecks[4];
 
 char *find_space(int c) {
     int s = 0;
@@ -35,13 +43,50 @@ char *find_space(int c) {
     return linePtr + ptr;
 }
 
+int get_rounds(int bot, int material, int ownedRes, bool botAdded) {
+    if (bp.neededMaterial[bot][material] == 0) return 0;
+    assert(bp.bots[material] != 0);
+    return (int) (
+            (float) (bp.neededMaterial[bot][material] - ownedRes + botAdded) / (float) (bp.bots[material] + botAdded) +
+            0.98);
+}
+
+Bottleneck get_bottleneck(int bot) {
+    Bottleneck res = {-1, -1, bot};
+    for (int i = 0; i < 3; ++i) {
+        int rounds = get_rounds(bot, i, bp.resources[i], false);
+        if (rounds > res.mRounds) {
+            res.mRounds = rounds;
+            res.mResource = i;
+        }
+    }
+    return res;
+}
+
+void create_bot(int bot) {
+    for (int i = 0; i < 3; ++i) {
+        bp.resources[i] -= bp.neededMaterial[bot][i];
+    }
+    bp.bots[bot]++;
+}
+
+int will_make_it_change(int bot1, int bot2) {
+    int mr = 0;
+    for (int j = 0; j < 3; ++j) {
+        int ownedRes = bp.resources[j] - bp.neededMaterial[bot1][j];
+        int rounds = get_rounds(bot2, j, ownedRes, j == bot1);
+        mr = rounds > mr ? rounds : mr;
+    }
+    return mr;
+}
+
 
 int main() {
     FILE *f = fopen("/home/tereza/CLionProjects/aoc2022/input.txt", "r");
     int sum = 0;
     while ((lineSize = getline(&linePtr, &n, f)) != -1) {
         count++;
-        Blueprint bp = {{0, 0, 0, 0}, 0, {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}}, {1, 0, 0, 0}};
+        bp = (Blueprint) {{0, 0, 0, 0}, 0, {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}}, {1, 0, 0, 0}};
         int c;
         c = (int) strtol(find_space(6), NULL, 10);
         bp.neededMaterial[0][0] = c;
@@ -58,68 +103,41 @@ int main() {
 
 
         for (int i = 0; i < 24; ++i) {
-            int rounds = 0;
-            int roundResource = -1;
-            if (bp.curRes < 3) {
-                bool good = true;
-                for (int j = 0; j < 3; ++j) {
-                    if (bp.resources[j] < bp.neededMaterial[bp.curRes + 1][j]) {
-                        good = false;
-                        int diff = (int) (
-                                (double) (bp.neededMaterial[bp.curRes + 1][j] - bp.resources[j]) / (double) bp.bots[j] +
-                                1.0);
-                        if (diff > rounds) {
-                            rounds = diff;
-                            roundResource = j;
+            Bottleneck bns[4];
+            btc = 0;
+            int neck = bp.curRes >= 3 ? bp.curRes : bp.curRes + 1;
+            while (true) {
+                Bottleneck bn = get_bottleneck(neck);
+                if (bn.mRounds == 0) {
+                    bool good = true;
+                    int very_good = btc == 0;
+                    for (int j = btc - 1; j >= 0; --j) {
+                        if (will_make_it_change(neck, bns[btc - 1].mParent) > bns[btc - 1].mRounds) {
+                            good = false;
+                        }
+                        if (will_make_it_change(neck, bns[btc - 1].mParent) < bns[btc - 1].mRounds) {
+                            very_good = true;
                         }
                     }
-                }
-                if (good) {
-                    bp.curRes++;
-                    for (int k = 0; k < 3; ++k) {
-                        bp.resources[k] -= bp.neededMaterial[bp.curRes][k];
-                    }
-                    // will be mistakenly added for this robot the next step
-                    bp.resources[bp.curRes]--;
-                    bp.bots[bp.curRes]++;
-                }
-            }
-            for (int p = 0; p <= bp.curRes; ++p) {
-                bool good = true;
-                for (int j = 0; j < 3; ++j) {
-                    if (bp.resources[j] < bp.neededMaterial[p][j]) {
-                        good = false;
-                    }
-                }
-                if (good) {
-                    bp.bots[p]++;
-                    int j = roundResource;
-                    if (bp.bots[j] < 1) {
-                        continue;
-                    }
-                    int tr = bp.resources[j] - bp.neededMaterial[p][j];
-                    int diff = (int) (
-                            (double) (bp.neededMaterial[bp.curRes + 1][j] - tr + 1) / (double) bp.bots[j] +
-                            1.0);
-                    // will make it faster
-                    if (abs(diff) < rounds) {
-                        for (int k = 0; k < 3; ++k) {
-                            bp.resources[k] -= bp.neededMaterial[p][k];
+                    if (good && very_good) {
+                        if (neck == bp.curRes+1) {
+                            bp.curRes++;
                         }
-                        // will be mistakenly added for this robot the next step
-                        bp.resources[p]--;
-                        goto if_end;
+                        create_bot(neck);
+                        bp.resources[neck]--;
                     }
-                    bp.bots[p]--;
+                    break;
                 }
-
+                bns[btc] = bn;
+                btc++;
+                neck = bn.mResource;
+                if (btc > 3) {
+                    break;
+                }
             }
-            if_end:
-            // ore za predchozi kolo
             for (int j = 0; j < 4; ++j) {
                 bp.resources[j] += bp.bots[j];
             }
-
         }
 
         sum += bp.resources[3] * count;
