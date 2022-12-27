7fef1e2ac42e28423b9055347b6c68bc603a5254
Tereza Sukupová <tereza.sukupova@anafra.cz>
2022-12-27 20:57:47 +0100

    21-2

--- main.c
+++ main.c
@@ -17,8 +17,13 @@ struct mNode {
     bool computed;
     char left[5];
     char right[5];
+    char *name;
+    bool lHuman;
+    bool rHuman;
     char op;
+    long rOrL;
 };
+
 
 Node *root;
 
@@ -29,10 +34,13 @@ int count = 0;
 
 GHashTable *ht;
 
+Node *human;
+
 
 long get_value(Node *x) {
     assert(x != NULL);
     if (x->computed) {
+        printf("%s %ld\n", x->name, x->value);
         return x->value;
     }
     long v1 = get_value(g_hash_table_lookup(ht, x->left));
@@ -51,11 +59,78 @@ long get_value(Node *x) {
             x->value = v1 / v2;
             break;
     }
-    printf("%ld %c %ld = %ld\n",v1,x->op,v2,x->value);
+    printf("%s %ld %c %ld = %ld\n", x->name, v1, x->op, v2, x->value);
     x->computed = true;
     return x->value;
+}
 
+bool is_human_column(Node *x) {
+    assert(x != NULL);
+    if (strcmp(x->name, "humn") == 0) {
+        x->lHuman = true;
+        x->rHuman = true;
+        return true;
+    }
+    if (x->computed) {
+        return false;
+    }
 
+    bool h1 = is_human_column(g_hash_table_lookup(ht, x->left));
+    bool h2 = is_human_column(g_hash_table_lookup(ht, x->right));
+    assert(!(h1 && h2));
+    if (h1) {
+        x->lHuman = true;
+    } else if (h2) {
+        x->rHuman = true;
+    }
+    return (h1 || h2);
+
+}
+
+long backtrace_human(Node *x) {
+    assert(x != NULL);
+    assert(x->lHuman || x->rHuman);
+    assert(x->computed);
+    // human with value from previous krok
+    if (x->lHuman && x->rHuman) {
+        return x->value;
+    }
+    Node *h;
+    long v;
+    if (x->lHuman) {
+       h =  g_hash_table_lookup(ht, x->left);
+        v = get_value(g_hash_table_lookup(ht, x->right));
+        switch (x->op) {
+            case '-':
+                x->rOrL = x->value + v;
+                break;
+            case '/':
+                x->rOrL = x->value * v;
+                break;
+        }
+    } else if (x->rHuman) {
+        v = get_value(g_hash_table_lookup(ht, x->left));
+        h = g_hash_table_lookup(ht, x->right);
+        switch (x->op) {
+            case '-':
+                x->rOrL = v - x->value;
+                break;
+            case '/':
+                x->rOrL = v / x->value;
+                break;
+        }
+    }
+    switch (x->op) {
+        case '+':
+            x->rOrL = x->value - v;
+            break;
+        case '*':
+            x->rOrL = x->value / v;
+            break;
+    }
+    h->value = x->rOrL;
+    h->computed = true;
+    return backtrace_human(h);
 }
 
 int main() {
@@ -66,6 +141,7 @@ int main() {
         char *name = calloc(sizeof(char), 5);
         strcpy(name, strtok(linePtr, ":"));
         Node *s = calloc(sizeof(Node), 1);
+        s->name = name;
         if (isdigit(linePtr[6])) {
             s->value = (long) strtol(linePtr + 6, NULL, 10);
             s->computed = true;
@@ -80,12 +156,27 @@ int main() {
         g_hash_table_insert(ht, name, s);
         if (strcmp(name, "root") == 0) {
             root = s;
+        } else if (strcmp(name, "humn") == 0) {
+            human = s;
         }
 
     }
 
+    long eq;
+    if (is_human_column(root)) {
+        if (root->lHuman) {
+            eq = get_value(g_hash_table_lookup(ht, root->right));
+        } else {
+            eq = get_value(g_hash_table_lookup(ht, root->left));
+        }
+        root->value = eq + eq;
+        root->computed = true;
+        root->rOrL = eq;
+        long w = backtrace_human(root);
+        printf("w = %ld\n", w);
+    }
 
-    printf("--- %ld ---", get_value(root));
+//    printf("--- %ld ---", get_value(root));
     free(linePtr);
     g_hash_table_destroy(ht);
 
