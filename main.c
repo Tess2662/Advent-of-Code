b11e7102c56919f300fcac2c85b75e2e9c7ab40d
Tereza Sukupová <tereza.sukupova@anafra.cz>
2022-12-30 16:42:43 +0100

    22-2

--- main.c
+++ main.c
@@ -36,9 +36,12 @@ int fCount = 0;
 char **grid;
 size_t columns = 0;
 size_t rows = 0;
-int cubeSize = 4;
+int cubeSize = 50;
 char *moves;
 int cCount = 0;
+int direction, row, column;
+Facing *curFace = &facings[0];
+
 
 void allocateGrid() {
     grid = calloc(rows, sizeof(char *));
@@ -62,6 +65,10 @@ void countLines() {
 
 void parseInput() {
     while ((lineSize = getline(&linePtr, &n, f)) != -1) {
+        if (count == 0) {
+            row = 0;
+            column = (int) strcspn(linePtr, ".");
+        }
         if (lineSize < 2) {
             break;
         }
@@ -100,27 +107,18 @@ void findSiblings() {
     }
 }
 
-bool containsFaceDst(Connection * c, Facing * f) {
-    for (int i = 0; i < 4; ++i) {
-        if (c[i].dst == f) {
-            return true;
-        }
-    }
-    return false;
-}
-
 void connectDisconnectedChilds(Facing *facing) {
     Facing *ff;
     Connection *cs = facing->cons;
-    Facing * first,*second;
+    Facing *first, *second;
     int firstOut, secondOut;
     int firstIn, secondIn;
     for (int i = 0; i < 4; ++i) {
         int j = (i + 1) % 4;
         first = cs[i].dst;
         second = cs[j].dst;
-        firstIn = (4 + cs[i].dir -1) % 4;
-        secondIn = (cs[j].dir +1) % 4;
+        firstIn = (4 + cs[i].dir - 1) % 4;
+        secondIn = (cs[j].dir + 1) % 4;
         firstOut = (firstIn + 2) % 4;
         secondOut = (secondIn + 2) % 4;
         if (first && second) {
@@ -128,10 +126,169 @@ void connectDisconnectedChilds(Facing *facing) {
                 assert(!second->cons[secondOut].dst);
                 first->cons[firstOut] = (Connection) {secondIn, second};
                 second->cons[secondOut] = (Connection) {firstIn, first};
-                cCount+=2;
+                cCount += 2;
             }
         }
 
+    }
+}
+
+bool isObstacle(int r, int c) {
+    return grid[r][c] == '#';
+}
+
+
+bool setNextCoords() {
+    int xInc = (1 - direction) % 2;
+    int yInc = (2 - direction) % 2;
+    // hopefully no need to check overflow
+    int newRow = ((int) rows + row + yInc) % (int) rows;
+    int newCol = ((int) columns + column + xInc) % (int) columns;
+    if (column / cubeSize != newCol / cubeSize ||
+        row / cubeSize != newRow / cubeSize) {
+        Connection *cn = &curFace->cons[direction];
+        int dstCol = cn->dst->x;
+        int dstRow = cn->dst->y;
+        int colFaceDiff = column - curFace->x;
+        int rowFaceDiff = row - curFace->y;
+        int cubeSizeMinusOne = cubeSize - 1;
+        switch (direction) {
+            case 0:
+                switch (cn->dir) {
+                    case 0:
+                        newCol = dstCol;
+                        newRow = dstRow + rowFaceDiff;
+                        break;
+                    case 1:
+                        newCol = dstCol + (cubeSizeMinusOne - rowFaceDiff);
+                        newRow = dstRow;
+                        break;
+                    case 2:
+                        newCol = dstCol + cubeSizeMinusOne;
+                        newRow = dstRow + (cubeSizeMinusOne - rowFaceDiff);
+                        printf("%d %d this shit shouldnot happen\n", direction, cn->dir);
+                        break;
+                    case 3:
+                        newCol = dstCol + rowFaceDiff;
+                        newRow = dstRow + cubeSizeMinusOne;
+                        break;
+                }
+                break;
+            case 1:
+                switch (cn->dir) {
+                    case 0:
+                        newCol = dstCol;
+                        newRow = dstRow + (cubeSizeMinusOne - colFaceDiff);
+                        break;
+                    case 1:
+                        newCol = dstCol + colFaceDiff;
+                        newRow = dstRow;
+                        break;
+                    case 2:
+                        newCol = dstCol + cubeSizeMinusOne;
+                        newRow = dstRow + colFaceDiff;
+                        break;
+                    case 3:
+                        newCol = dstCol + (cubeSizeMinusOne - colFaceDiff);
+                        newRow = dstRow + cubeSizeMinusOne;
+                        printf("%d %d this shit shouldnot happen\n", direction, cn->dir);
+                        break;
+                }
+                break;
+            case 2:
+                switch (cn->dir) {
+                    case 0:
+                        newCol = dstCol;
+                        newRow = dstRow + (cubeSizeMinusOne - rowFaceDiff);
+                        printf("%d %d this shit shouldnot happen\n", direction, cn->dir);
+                        break;
+                    case 1:
+                        newCol = dstCol + rowFaceDiff;
+                        newRow = dstRow;
+                        break;
+                    case 2:
+                        newCol = dstCol + cubeSizeMinusOne;
+                        newRow = dstRow + rowFaceDiff;
+                        break;
+                    case 3:
+                        newCol = dstCol + (cubeSizeMinusOne - rowFaceDiff);
+                        newRow = dstCol + cubeSizeMinusOne;
+                        break;
+                }
+                break;
+            case 3:
+                switch (cn->dir) {
+                    case 0:
+                        newCol = dstCol;
+                        newRow = dstRow + colFaceDiff;
+                        break;
+                    case 1:
+                        printf("%d %d this shit shouldnot happen\n", direction, cn->dir);
+                        newRow = dstRow;
+                        newCol = dstCol + (cubeSizeMinusOne - colFaceDiff);
+                        break;
+                    case 2:
+                        newCol = dstCol + cubeSizeMinusOne;
+                        newRow = dstRow + (cubeSizeMinusOne - rowFaceDiff);
+                        break;
+                    case 3:
+                        newRow = dstRow + cubeSizeMinusOne;
+                        newCol = dstCol + colFaceDiff;
+                        break;
+                }
+                break;
+        }
+        if (isObstacle(newRow, newCol)) {
+            return false;
+        }
+        direction = cn->dir;
+        curFace = cn->dst;
+//        xInc = (1 - direction) % 2;
+//        yInc = (2 - direction) % 2;
+    }
+    if (isObstacle(newRow, newCol)) {
+        return false;
+    }
+    row = newRow;
+    column = newCol;
+    char dirSign = '.';
+    switch (direction) {
+        case 0:
+            dirSign = '>';
+            break;
+        case 1:
+            dirSign = 'v';
+            break;
+        case 2:
+            dirSign = '<';
+            break;
+        case 3:
+            dirSign = '^';
+            break;
+    }
+    grid[row][column] = dirSign;
+    return true;
+}
+
+void parseMoves() {
+    char *move = moves;
+    int moveCount = 0;
+    while (*move != 0) {
+        moveCount = (int) strtol(move, &move, 10);
+        while (moveCount > 0 && setNextCoords()) {
+            moveCount--;
+        }
+        if (*move != 0) {
+            if (*move == 'R') {
+                direction = (direction + 1) % 4;
+            } else if (*move == 'L') {
+                direction = (4 + direction - 1) % 4;
+            } else {
+                printf("move %s\n", move);
+                exit(123);
+            }
+            move++;
+        }
     }
 }
 
@@ -142,6 +299,21 @@ void destroy() {
     }
     free(grid);
     free(linePtr);
+}
+
+void checkCube() {
+    Facing *ff;
+    Connection *c;
+    for (int i = 0; i < fCount; ++i) {
+        ff = &facings[i];
+        c = facings[i].cons;
+        for (int j = 0; j < 4; ++j) {
+            assert(c[j].dst->cons[(c[j].dir + 2) % 4].dst == ff);
+            for (int k = j + 1; k < 4; ++k) {
+                assert(c[j].dst != c[k].dst);
+            }
+        }
+    }
 }
 
 int main() {
@@ -158,18 +330,15 @@ int main() {
         }
     }
 
-    Facing *ff;
-    Connection *c;
-    for (int i = 0; i < fCount; ++i) {
-        ff = &facings[i];
-        c = facings[i].cons;
-        for (int j = 0; j < 4; ++j) {
-            assert(c[j].dst->cons[(c[j].dir + 2)%4].dst == ff);
-            for (int k = j+1; k < 4; ++k) {
-                assert(c[j].dst != c[k].dst);
-            }
-        }
+    checkCube();
+    parseMoves();
+
+    for (int i = 0; i < rows; ++i) {
+        printf("%s\n", grid[i]);
     }
+
+
+    printf("help %d\n", 1000 * (row + 1) + 4 * (column + 1) + direction);
 
 
     destroy();
