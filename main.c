76a99381cc0e9d9002dcf24a9bd7e4003e545733
Tereza Sukupová <tereza.sukupova@anafra.cz>
2022-12-30 09:24:49 +0100

    22-2 cube builded

--- main.c
+++ main.c
@@ -10,175 +10,168 @@
 #include <assert.h>
 #include <math.h>
 
-typedef struct mNode Node;
-
-struct mNode {
-    long value;
-    bool computed;
-    char left[5];
-    char right[5];
-    char *name;
-    bool lHuman;
-    bool rHuman;
-    char op;
-    long rOrL;
-};
-
-
-Node *root;
-
 char *linePtr = NULL;
 size_t n = 0;
 size_t lineSize;
 int count = 0;
-
-GHashTable *ht;
-
-Node *human;
+FILE *f;
 
 
-long get_value(Node *x) {
-    assert(x != NULL);
-    if (x->computed) {
-        printf("%s %ld\n", x->name, x->value);
-        return x->value;
+typedef struct mFacing Facing;
+typedef struct mConnection Connection;
+
+struct mConnection {
+    int dir;
+    Facing *dst;
+};
+struct mFacing {
+    int x;
+    int y;
+    // 0 - right, down, left, up
+    Connection cons[4];
+};
+
+Facing facings[6];
+int fCount = 0;
+char **grid;
+size_t columns = 0;
+size_t rows = 0;
+int cubeSize = 4;
+char *moves;
+int cCount = 0;
+
+void allocateGrid() {
+    grid = calloc(rows, sizeof(char *));
+    for (int i = 0; i < rows; ++i) {
+        grid[i] = calloc(columns + 1, sizeof(char));
     }
-    long v1 = get_value(g_hash_table_lookup(ht, x->left));
-    long v2 = get_value(g_hash_table_lookup(ht, x->right));
-    switch (x->op) {
-        case '+':
-            x->value = v1 + v2;
-            break;
-        case '-':
-            x->value = v1 - v2;
-            break;
-        case '*':
-            x->value = v1 * v2;
-            break;
-        case '/':
-            x->value = v1 / v2;
-            break;
-    }
-    printf("%s %ld %c %ld = %ld\n", x->name, v1, x->op, v2, x->value);
-    x->computed = true;
-    return x->value;
-}
-
-bool is_human_column(Node *x) {
-    assert(x != NULL);
-    if (strcmp(x->name, "humn") == 0) {
-        x->lHuman = true;
-        x->rHuman = true;
-        return true;
-    }
-    if (x->computed) {
-        return false;
-    }
-
-    bool h1 = is_human_column(g_hash_table_lookup(ht, x->left));
-    bool h2 = is_human_column(g_hash_table_lookup(ht, x->right));
-    assert(!(h1 && h2));
-    if (h1) {
-        x->lHuman = true;
-    } else if (h2) {
-        x->rHuman = true;
-    }
-    return (h1 || h2);
 
 }
 
-long backtrace_human(Node *x) {
-    assert(x != NULL);
-    assert(x->lHuman || x->rHuman);
-    assert(x->computed);
-    // human with value from previous krok
-    if (x->lHuman && x->rHuman) {
-        return x->value;
-    }
-    Node *h;
-    long v;
-    if (x->lHuman) {
-       h =  g_hash_table_lookup(ht, x->left);
-        v = get_value(g_hash_table_lookup(ht, x->right));
-        switch (x->op) {
-            case '-':
-                x->rOrL = x->value + v;
-                break;
-            case '/':
-                x->rOrL = x->value * v;
-                break;
+void countLines() {
+    while ((lineSize = getline(&linePtr, &n, f)) != -1) {
+        if (lineSize < 2) {
+            break;
         }
-    } else if (x->rHuman) {
-        v = get_value(g_hash_table_lookup(ht, x->left));
-        h = g_hash_table_lookup(ht, x->right);
-        switch (x->op) {
-            case '-':
-                x->rOrL = v - x->value;
-                break;
-            case '/':
-                x->rOrL = v / x->value;
-                break;
+        rows++;
+        if (lineSize - 1 > columns) {
+            columns = lineSize - 1;
         }
     }
-    switch (x->op) {
-        case '+':
-            x->rOrL = x->value - v;
+}
+
+void parseInput() {
+    while ((lineSize = getline(&linePtr, &n, f)) != -1) {
+        if (lineSize < 2) {
             break;
-        case '*':
-            x->rOrL = x->value / v;
-            break;
+        }
+        linePtr[strcspn(linePtr, "\n\r")] = 0;
+        strncpy(grid[count], linePtr, columns);
+        if (count % cubeSize == 0) {
+            for (int i = 0; i < columns; i += cubeSize) {
+                if (linePtr[i] == '.' || linePtr[i] == '#') {
+                    facings[fCount] = (Facing) {i, count};
+                    fCount++;
+                }
+            }
+        }
+        count++;
+
     }
-    h->value = x->rOrL;
-    h->computed = true;
-    return backtrace_human(h);
+    lineSize = getline(&linePtr, &n, f);
+    moves = calloc(lineSize + 1, sizeof(char));
+    strncpy(moves, linePtr, lineSize + 1);
+
+}
+
+void findSiblings() {
+    for (int i = 0; i < fCount; ++i) {
+        for (int j = i + 1; j < fCount; ++j) {
+            if (facings[j].x - facings[i].x == cubeSize && facings[j].y == facings[i].y) {
+                facings[i].cons[0] = (Connection) {0, &facings[j]};
+                facings[j].cons[2] = (Connection) {2, &facings[i]};
+                cCount += 2;
+            } else if (facings[j].y - facings[i].y == cubeSize && facings[j].x == facings[i].x) {
+                cCount += 2;
+                facings[i].cons[1] = (Connection) {1, &facings[j]};
+                facings[j].cons[3] = (Connection) {3, &facings[i]};
+            }
+        }
+    }
+}
+
+bool containsFaceDst(Connection * c, Facing * f) {
+    for (int i = 0; i < 4; ++i) {
+        if (c[i].dst == f) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void connectDisconnectedChilds(Facing *facing) {
+    Facing *ff;
+    Connection *cs = facing->cons;
+    Facing * first,*second;
+    int firstOut, secondOut;
+    int firstIn, secondIn;
+    for (int i = 0; i < 4; ++i) {
+        int j = (i + 1) % 4;
+        first = cs[i].dst;
+        second = cs[j].dst;
+        firstIn = (4 + cs[i].dir -1) % 4;
+        secondIn = (cs[j].dir +1) % 4;
+        firstOut = (firstIn + 2) % 4;
+        secondOut = (secondIn + 2) % 4;
+        if (first && second) {
+            if (!first->cons[firstOut].dst) {
+                assert(!second->cons[secondOut].dst);
+                first->cons[firstOut] = (Connection) {secondIn, second};
+                second->cons[secondOut] = (Connection) {firstIn, first};
+                cCount+=2;
+            }
+        }
+
+    }
+}
+
+void destroy() {
+    free(moves);
+    for (int i = 0; i < rows; ++i) {
+        free(grid[i]);
+    }
+    free(grid);
+    free(linePtr);
 }
 
 int main() {
-    ht = g_hash_table_new(g_str_hash, g_str_equal);
-    FILE *f = fopen("/home/tereza/CLionProjects/aoc2022/input.txt", "r");
-    while ((lineSize = getline(&linePtr, &n, f)) != -1) {
-        linePtr[strcspn(linePtr, "\r\n")] = '\0';
-        char *name = calloc(sizeof(char), 5);
-        strcpy(name, strtok(linePtr, ":"));
-        Node *s = calloc(sizeof(Node), 1);
-        s->name = name;
-        if (isdigit(linePtr[6])) {
-            s->value = (long) strtol(linePtr + 6, NULL, 10);
-            s->computed = true;
-        } else {
-            strtok(NULL, " ");
-            strtok(NULL, " ");
-            strcpy(s->left, linePtr + 6);
-            s->op = linePtr[11];
-            strcpy(s->right, linePtr + 13);
-            s->computed = false;
-        }
-        g_hash_table_insert(ht, name, s);
-        if (strcmp(name, "root") == 0) {
-            root = s;
-        } else if (strcmp(name, "humn") == 0) {
-            human = s;
-        }
+    f = fopen("/home/tereza/CLionProjects/aoc2022/input.txt", "r");
+    countLines();
+    allocateGrid();
+    rewind(f);
+    parseInput();
+    findSiblings();
 
+    while (cCount < 24) {
+        for (int i = 0; i < fCount; ++i) {
+            connectDisconnectedChilds(&facings[i]);
+        }
     }
 
-    long eq;
-    if (is_human_column(root)) {
-        if (root->lHuman) {
-            eq = get_value(g_hash_table_lookup(ht, root->right));
-        } else {
-            eq = get_value(g_hash_table_lookup(ht, root->left));
+    Facing *ff;
+    Connection *c;
+    for (int i = 0; i < fCount; ++i) {
+        ff = &facings[i];
+        c = facings[i].cons;
+        for (int j = 0; j < 4; ++j) {
+            assert(c[j].dst->cons[(c[j].dir + 2)%4].dst == ff);
+            for (int k = j+1; k < 4; ++k) {
+                assert(c[j].dst != c[k].dst);
+            }
         }
-        root->value = eq + eq;
-        root->computed = true;
-        root->rOrL = eq;
-        long w = backtrace_human(root);
-        printf("w = %ld\n", w);
     }
 
-//    printf("--- %ld ---", get_value(root));
-    free(linePtr);
-    g_hash_table_destroy(ht);
 
+    destroy();
     return 0;
 }
